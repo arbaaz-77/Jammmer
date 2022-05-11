8ae00efaedf26d21608c653412b4b86c8b4cf991
Arbaaz Sumar <43441056+arbaaz-77@users.noreply.github.com>
2022-05-11 15:12:02 +0300

    Update Spotify.js

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -22,7 +22,7 @@ const Spotify = {
 
       return accessToken;
     } else {
-      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUrl}`;
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public`;
       window.location = accessUrl;
     }
   },
